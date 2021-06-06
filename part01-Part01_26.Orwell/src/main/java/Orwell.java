764ea76425c27e53323a828f865ea9c8933cce48
julianbooher <julianbooher@hotmail.com>
2021-06-05 20:07:08 -0500

    finished pset 26

--- part01-Part01_26.Orwell/src/main/java/Orwell.java
+++ part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 public class Orwell {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give a number:");
+        int speed = Integer.valueOf(scanner.nextLine());
+        if (speed == 1984){
+            System.out.println("Orwell");
+        }
     }
 }
