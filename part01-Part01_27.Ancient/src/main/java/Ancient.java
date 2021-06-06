ebd31d707e999df48051c2fb20c2312df0b858cb
julianbooher <julianbooher@hotmail.com>
2021-06-05 20:10:59 -0500

    finished pset 27

--- part01-Part01_27.Ancient/src/main/java/Ancient.java
+++ part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 public class Ancient {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give a year:");
+        int year = Integer.valueOf(scanner.nextLine());
+        if (year < 2015){
+            System.out.println("Ancient history!");
+        }        
     }
 }
