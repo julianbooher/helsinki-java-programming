b914aa82590a36ee5e2492303e188cba2ac3a32b
julianbooher <julianbooher@hotmail.com>
2021-06-09 13:23:16 -0500

    finished pset 32

--- part01-Part01_34.Same/src/main/java/Same.java
+++ part01-Part01_34.Same/src/main/java/Same.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 public class Same {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // Write your program here. 
+        System.out.println("Enter the first string:");
+        String first = scanner.nextLine(); 
+        System.out.println("Enter the second string:");
+        String second = scanner.nextLine(); 
+        
+        if(first.equals(second)){
+            System.out.println("Same");
+        } else{
+            System.out.println("Different");
+        }
     }
 }
