c49fd0bbbd1fd7b41c036b0791629988f68155da
julianbooher <julianbooher@hotmail.com>
2021-06-09 13:11:20 -0500

    finished pset 33

--- part01-Part01_33.Password/src/main/java/Password.java
+++ part01-Part01_33.Password/src/main/java/Password.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 public class Password {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // Write your program here 
+        System.out.println("Password?");
+        String password = scanner.nextLine();   
+        
+        if (password.equals("Caput Draconis")){
+            System.out.println("Welcome!");
+        } else{
+            System.out.println("Off with you!");
+        }        
     }
 }
