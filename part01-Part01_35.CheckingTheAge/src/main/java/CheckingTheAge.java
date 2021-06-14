6317a90a089c80c3915b1771b141a5d3a116d27c
julianbooher <julianbooher@hotmail.com>
2021-06-14 18:47:05 -0500

    finished pset 35

--- part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
+++ part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 public class CheckingTheAge {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How old are you?");
+        int age = Integer.valueOf(scanner.nextLine());   
+        
+        if (age >= 0 && age <= 120){
+            System.out.println("OK");
+        } else {
+            System.out.println("Impossible!");
+        }
+        
     }
 }
