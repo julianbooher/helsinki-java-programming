5341010b79c5747c8758cf4922b190254e90561a
julianbooher <julianbooher@hotmail.com>
2021-06-15 20:25:44 -0500

    finished pset 2.2

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,8 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        // Declaring the variables and assigning user input to them
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println(first * first);
     }
 }
--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,10 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        double first = Integer.valueOf(scanner.nextLine());
+        double second = Integer.valueOf(scanner.nextLine());
+        System.out.println(Math.sqrt(first + second));
+        
+        
     }
 }
