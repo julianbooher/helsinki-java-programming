0e7b6edc9bc496cace920bbf26833f94cb304435
julianbooher <julianbooher@hotmail.com>
2021-06-15 20:27:41 -0500

    finished pset 2.3

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -5,6 +5,12 @@ public class AbsoluteValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double first = Integer.valueOf(scanner.nextLine());
+        if (first >= 0){
+            System.out.println(first);
+        } else{
+            System.out.println(first * -1);
+        }
 
     }
 }
