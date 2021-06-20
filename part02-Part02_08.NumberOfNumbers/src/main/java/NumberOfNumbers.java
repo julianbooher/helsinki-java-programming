738d5ffb4212f06ab8bb32c1257eca876cc9355d
julianbooher <julianbooher@hotmail.com>
2021-06-20 13:52:55 -0500

    finished pset 2.8

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,20 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int count = 0;
+        
+        while(true){
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == 0){
+                break;
+            } else {
+                count += 1;
+            }
+        }
+        
+        System.out.println("Number of numbers: " + count);
+        
     }
 }
