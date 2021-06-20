93cb7952fd5b9111dc65cf3d1505b14ead5cbaee
julianbooher <julianbooher@hotmail.com>
2021-06-20 14:01:06 -0500

    finished pset 2.11

--- part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -5,6 +5,23 @@ public class NumberAndSumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int count = 0;
+        int sum = 0;
+        
+        while(true){
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == 0){
+                break;
+            } else {
+                sum += num;
+                count += 1;
+            }
+        }
+        
+        System.out.println("Number of numbers: " + count);
+        System.out.println("Sum of the numbers: " + sum);
+        
+        
     }
 }
