14db2cbf988a9ccf735d74ff5dd99424b87a4f55
julianbooher <julianbooher@hotmail.com>
2021-06-20 15:23:13 -0500

    finished pset 2.12

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,27 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int count = 0;
+        int sum = 0;
+        
+        while(true){
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == 0){
+                break;
+            } else {
+                sum += num;
+                count += 1;
+            }
+        }
+        
+        if (sum < 0){
+            sum = sum * -1;
+            System.out.println("Average of the numbers: " + ((double) sum / count) * -1);
+            
+        } else {
+            System.out.println("Average of the numbers: " + ((double) sum / count));
+            
+        }
     }
 }
