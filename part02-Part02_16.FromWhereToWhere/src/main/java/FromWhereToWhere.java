a0c14630d6496a5d72b5d3b61a055d118ca3f6ea
julianbooher <julianbooher@hotmail.com>
2021-06-21 18:05:26 -0500

    finished pset 2.16

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 public class FromWhereToWhere {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Where to?");
+        int to = Integer.valueOf(scanner.nextLine());
+        System.out.println("Where from?");
+        int from = Integer.valueOf(scanner.nextLine());
+        if (from <= to){
+            for (int i = from; i <= to; i++){
+                System.out.println(i);
+            }
+        }
+        
         // Write your program here
     }
 }
