302b48738c620781504b0a45f945a4278ba95884
julianbooher <julianbooher@hotmail.com>
2021-06-21 17:51:07 -0500

    finished pset 2.13

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,27 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+        
+        while(true){
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == 0){
+                break;
+            } else if (num > 0) {
+                sum += num;
+                count += 1;
+            }
+        }
+        
+        if (sum == 0){
+            System.out.println("Cannot calculate the average.");
+            
+        } else {
+            System.out.println("Average of the numbers: " + ((double) sum / count));
+            
+        }
 
     }
 }
