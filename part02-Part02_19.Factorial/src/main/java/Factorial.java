31dc4e0c62b0a535a679ab7fa7f726e17cd4cf03
julianbooher <julianbooher@hotmail.com>
2021-06-22 19:19:12 -0500

    finished pset 2.19

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,19 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int num = Integer.valueOf(scanner.nextLine());
+        
+        int factorial = 1;
+        if (num == 0){
+            factorial = 1;
+        } else {
+            for (int i = 1; i <= num; i++){
+                factorial *= i;
+            }
+        }
+        
+        System.out.println("The sum is: " + factorial);
 
     }
 }
