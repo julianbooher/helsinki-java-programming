09c6fdb34560e5b21bf3a76498cd965a498b6d92
julianbooher <julianbooher@hotmail.com>
2021-06-22 19:38:24 -0500

    finished pset 2.20

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -13,6 +13,39 @@ public class RepeatingBreakingAndRemembering {
         // the parts you haven't done, but you'll get points for the finished parts.
         
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give numbers:");
+        
+        int sum = 0;
+        int numbers = 0;
+        int even = 0;
+        int odd = 0;
+        
+
+        while (true){
+            int num = Integer.valueOf(scanner.nextLine());
+            
+            
+            
+            if (num == -1){
+                break;
+            } else {
+                if (num % 2 == 0){
+                    even++;
+                } else {
+                    odd++;
+                }
+                numbers ++;
+                sum += num;
+                continue;
+            }
+        }
+        
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + numbers);
+        System.out.println("Average: " + ((double)sum / numbers));
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
 
     }
 }
