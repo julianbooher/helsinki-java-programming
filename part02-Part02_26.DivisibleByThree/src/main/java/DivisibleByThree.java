9ece5faff7c6808e553a37a112e9248507ca2169
julianbooher <julianbooher@hotmail.com>
2021-06-26 15:40:24 -0500

    finished pset 2.26

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -7,5 +7,11 @@ public class DivisibleByThree {
         Scanner scanner = new Scanner(System.in);
 
     }
-
+    public static void divisibleByThreeInRange(int a, int b){
+        for(int i = a; i <= b; i++){
+            if (i % 3 == 0){
+                System.out.println(i);
+            }
+        }
+    }
 }
