de8b3291294026a55b3d84799170d7795db6e178
julianbooher <julianbooher@hotmail.com>
2021-06-27 23:59:06 -0500

    pset 2.34 finished

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -1,30 +1,46 @@
 
 public class AdvancedAstrology {
 
-    public static String printStars(int number) {
+    public static void printStars(int number) {
         // part 1 of the exercise
         String returnString = "";
         for(int i = 0; i < number; i++){
             returnString += "*";
         }
-        return returnString;
+        System.out.println(returnString);
     }
 
-    public static String printSpaces(int number) {
+    public static void printSpaces(int number) {
         // part 1 of the exercise
         String returnString = "";
         for(int i = 0; i < number; i++){
             returnString += " ";
         }
-        return returnString;    
+        System.out.print(returnString);    
     }
 
     public static void printTriangle(int size) {
         // part 2 of the exercise
+        for (int i = 1; i <= size; i++){
+            printSpaces(size - i);
+            printStars(i);
+        }
     }
 
     public static void christmasTree(int height) {
         // part 3 of the exercise
+        for (int i = 0; i < height; i++){
+            printSpaces(height - 1 - i);
+            printStars(i*2 + 1);
+        }
+        int i = 0;
+        while(i < 2){
+            printSpaces(height - 2);
+            printStars(3);
+            i++;
+        }
+
+
     }
 
     public static void main(String[] args) {
